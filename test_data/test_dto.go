7d43e43b2d692e606d74148066f97b196caed4ee
Petr Karmashev <smonkl@bk.ru>
2022-07-15 22:51:34 +0300

    #8 Unexported field removed

--- test_data/test_dto.go
+++ test_data/test_dto.go
@@ -3,8 +3,7 @@ package test_data
 import "time"
 
 type TestDTO struct {
-	Id         int64     `db:"id"`
-	Value      string    `db:"value"`
-	Time       time.Time `db:"time"`
-	unexported string
+	Id    int64     `db:"id"`
+	Value string    `db:"value"`
+	Time  time.Time `db:"time"`
 }
